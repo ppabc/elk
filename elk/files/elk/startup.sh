73546503b54cb0a48c6eae0cfe0abc9efbb4c7e8
ppabc <ppabc@qq.com>
2018-06-12 14:19:18 +0800

    update

--- elk/files/elk/startup.sh
+++ elk/files/elk/startup.sh
@@ -3,7 +3,7 @@ echo "Starting elasticsearch"
 /opt/elasticsearch/bin/elasticsearch &
 sleep 10
 echo "Starting logstash"
-/opt/logstash/bin/logstash -f /opt/config/conf.d/logstash.conf &
+/opt/logstash/bin/logstash -f /elk/config/logstash.conf &
 sleep 10
 echo "Starting kibana"
 /opt/kibana/bin/kibana
