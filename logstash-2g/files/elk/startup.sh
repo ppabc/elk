33b406fdd16488303d5a82aacfb63a3c87521e77
ppabc <ppabc@qq.com>
2018-06-14 10:10:04 +0800

    update

--- logstash-2g/files/elk/startup.sh
+++ logstash-2g/files/elk/startup.sh
@@ -2,8 +2,6 @@
 sed -i "s/Xms1g/Xms512m/g" /opt/logstash/config/jvm.options
 sed -i "s/Xmx1g/Xmx512m/g" /opt/logstash/config/jvm.options
 
-/opt/logstash/bin/logstash-plugin  install logstash-codec-netflow
-/opt/logstash/bin/logstash-plugin  install logstash-filter-ruby
 echo "Starting logstash"
 /opt/logstash/bin/logstash -f /elk/config/logstash.conf &
 
