d3abe0ba5ed8d5d18e0f3c56ca2dc5f781aa43d7
ppabc <ppabc@qq.com>
2018-06-14 10:23:37 +0800

    update

--- logstash-2g/files/elk/startup.sh
+++ logstash-2g/files/elk/startup.sh
@@ -3,5 +3,5 @@ sed -i "s/Xms1g/Xms512m/g" /opt/logstash/config/jvm.options
 sed -i "s/Xmx1g/Xmx512m/g" /opt/logstash/config/jvm.options
 
 echo "Starting logstash"
-/opt/logstash/bin/logstash -f /elk/config/logstash.conf &
+/opt/logstash/bin/logstash -f /elk/config/logstash.conf
 
